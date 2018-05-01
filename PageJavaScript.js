734dc68eda395bb2fea18e35d8d432f50720645f
chfgy5@mail.missouri.edu <chfgy5@mail.missouri.edu>
2018-04-30 21:52:04 -0500

    Seperated login and register page. Adding password requirements for register.

--- PageJavaScript.js
+++ PageJavaScript.js
@@ -28,7 +28,7 @@ function toggleSelected(number) {
     if (chk.checked == true && isEnlarged) {
         // If just checked true
         document.getElementById("text"+number).style.display = "block";
-        
+
     } else {
         // if just checked false
         document.getElementById("text"+number).style.display = "none";
@@ -57,11 +57,11 @@ checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
         items.classList.remove('visible');
         items.style.display = "none";
     }
-            
+
     else {
         items.classList.add('visible');
         items.style.display = "block";
-    }           
+    }
 }
 
 items.onblur = function(evt) {
@@ -81,3 +81,60 @@ function toggleNews() {
     toggle = !toggle;
 }
 
+var password = document.getElementById("newPassword");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the newPasswordRequirements box
+newPassword.onfocus = function() {
+  document.getElementById("newPasswordRequirements").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the newPasswordRequirements box
+newPassword.onblur = function() {
+  document.getElementById("newPasswordRequirements").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+newPassword.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(newPassword.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(newPassword.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(newPassword.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(newPassword.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
