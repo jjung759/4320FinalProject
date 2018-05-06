4943f44b6d960570dfe89d0f54203b4cdf47805a
chfgy5@mail.missouri.edu <chfgy5@mail.missouri.edu>
2018-05-06 14:09:30 -0500

    Prevented submit until fields filled correctly.

--- PageJavaScript.js
+++ PageJavaScript.js
@@ -1,4 +1,4 @@
-var oldHeight = 20;
+/*var oldHeight = 20;
 var isEnlarged = false;
 var checkList = document.getElementById('sourcelist');
 var items = document.getElementById('items');
@@ -79,14 +79,9 @@ function toggleNews() {
     if (!toggle) document.getElementById("login-message-fav").style.display = "none";
     else if (toggle) document.getElementById("login-message-fav").style.display = "block";
     toggle = !toggle;
-}
+}*/
 
-var password = document.getElementById("newPassword");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
-
+/* annoying to me. can use if desired.
 // When the user clicks on the password field, show the newPasswordRequirements box
 newPassword.onfocus = function() {
   document.getElementById("newPasswordRequirements").style.display = "block";
@@ -95,46 +90,70 @@ newPassword.onfocus = function() {
 // When the user clicks outside of the password field, hide the newPasswordRequirements box
 newPassword.onblur = function() {
   document.getElementById("newPasswordRequirements").style.display = "none";
-}
+}*/
 
 // When the user starts to type something inside the password field
-newPassword.onkeyup = function() {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(newPassword.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
+function passwordRequirements(){
+    var password = document.getElementById("newPassword");
+    var letter = document.getElementById("letter");
+    var capital = document.getElementById("capital");
+    var number = document.getElementById("number");
+    var length = document.getElementById("length");
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(newPassword.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(newPassword.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(newPassword.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    // Validate length
+    if(newPassword.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
 }
 
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(newPassword.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if(newPassword.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  // Validate length
-  if(newPassword.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
+function confirmPassword() {
+    var password = document.getElementById("newPassword");
+    var passwordConfirm = document.getElementById("passwordConfirm");
+    console.log("password: " + password.value + "\npasswordConfirm: " + passwordConfirm.value);
+    if(password.value != passwordConfirm.value){
+        //document.getElementById("passwordNotMatchMessage").classList.remove("noDisplay");
+        document.getElementById("passwordNotMatchMessage").style.display = "block";
+    } else {
+        document.getElementById("passwordNotMatchMessage").style.display = "none";
+        if(document.getElementById("length").classList.contains("valid") &&
+            document.getElementById("number").classList.contains("valid") &&
+            document.getElementById("capital").classList.contains("valid") &&
+            document.getElementById("letter").classList.contains("valid") &&
+            document.getElementById("username").value != "" ){
+                document.getElementById("registerSubmit").disabled = false;
+            }
+    }
 }
